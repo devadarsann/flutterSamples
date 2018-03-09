bb3000009e6a3c3ea4b43544f279f1cb6d1b664c
devadarsan <devadarsan.nr@letmedoit.in>
2018-03-09 10:05:26 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new FriendlyChatApp());
+}
+
+class FriendlyChatApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(title: "FriendlyChatApp",home: new ChatScreen(),);
+  }
+
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textEditingController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(appBar: new AppBar(title: new Text("Friendly Chat"),),
+    body: _buildTextComposer(),);
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(data: new IconThemeData(
+        color: Theme
+            .of(context)
+            .accentColor
+    ), child: new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new Row(
+        children: <Widget>[
+          new Flexible(child: new TextField(
+            controller: _textEditingController,
+            onSubmitted: _handleSubmitted,
+            decoration: new InputDecoration.collapsed(
+                hintText: "Send message"),),),
+          new Container(
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),
+            child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textEditingController.text)),
+          )
+        ],
+      ),
+    ));
+  }
+
+
+  void _handleSubmitted(String text){
+    _textEditingController.clear();
+  }
+
+}
